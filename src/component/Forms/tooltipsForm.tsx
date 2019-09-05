c199127087fc5c421225469227b2a04c99f1d23a
Mahdi Hellal <mahdihellal@gmail.com>
2019-09-05 12:47:13 +0100

    add tooltips modal for empty fields

--- src/component/Forms/tooltipsForm.tsx
+++ src/component/Forms/tooltipsForm.tsx
@@ -4,6 +4,11 @@ import { ICompetence, Theme, Activity } from "requests";
 import Button from "@material-ui/core/Button/Button";
 import { withStyles } from "@material-ui/core/styles";
 import createStyles from "@material-ui/core/styles/createStyles";
+import Dialog from "@material-ui/core/Dialog/";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
 
 interface Props {
   competences: ICompetence[];
@@ -21,20 +26,21 @@ export interface SubmitParamsTootltip {
   activities?: Activity[];
   parentId?: string;
   required?: { _id: string; title: string }[];
-  tooltips: {_id?: string; competenceId?: string; tooltip: string }[];
+  tooltips: { _id?: string; competenceId?: string; tooltip: string }[];
 }
 
 interface State {
-  comp1: {_id?: string; competenceId?: string; tooltip: string };
-  comp2: {_id?: string; competenceId?: string; tooltip: string };
-  comp3: {_id?: string; competenceId?: string; tooltip: string };
-  comp4: {_id?: string; competenceId?: string; tooltip: string };
-  comp5: {_id?: string; competenceId?: string; tooltip: string };
-  comp6: {_id?: string; competenceId?: string; tooltip: string };
-  comp7: {_id?: string; competenceId?: string; tooltip: string };
-  comp8: {_id?: string; competenceId?: string; tooltip: string };
-  comp9: {_id?: string; competenceId?: string; tooltip: string };
-  comp10: {_id?: string; competenceId?: string; tooltip: string };
+  comp1: { _id?: string; competenceId?: string; tooltip: string };
+  comp2: { _id?: string; competenceId?: string; tooltip: string };
+  comp3: { _id?: string; competenceId?: string; tooltip: string };
+  comp4: { _id?: string; competenceId?: string; tooltip: string };
+  comp5: { _id?: string; competenceId?: string; tooltip: string };
+  comp6: { _id?: string; competenceId?: string; tooltip: string };
+  comp7: { _id?: string; competenceId?: string; tooltip: string };
+  comp8: { _id?: string; competenceId?: string; tooltip: string };
+  comp9: { _id?: string; competenceId?: string; tooltip: string };
+  comp10: { _id?: string; competenceId?: string; tooltip: string };
+  open: boolean;
 }
 
 class TooltipForm extends React.Component<Props, State> {
@@ -42,53 +48,64 @@ class TooltipForm extends React.Component<Props, State> {
     comp1: (this.props.theme.tooltips && this.props.theme.tooltips[0]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[0].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[0]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[0]._id
     },
     comp2: (this.props.theme.tooltips && this.props.theme.tooltips[1]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[1].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[1]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[1]._id
     },
     comp3: (this.props.theme.tooltips && this.props.theme.tooltips[2]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[2].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[2]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[2]._id
     },
     comp4: (this.props.theme.tooltips && this.props.theme.tooltips[3]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[3].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[3]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[3]._id
     },
     comp5: (this.props.theme.tooltips && this.props.theme.tooltips[4]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[4].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[4]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[4]._id
     },
     comp6: (this.props.theme.tooltips && this.props.theme.tooltips[5]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[5].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[5]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[5]._id
     },
     comp7: (this.props.theme.tooltips && this.props.theme.tooltips[6]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[6].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[6]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[6]._id
     },
     comp8: (this.props.theme.tooltips && this.props.theme.tooltips[7]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[7].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[7]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[7]._id
     },
     comp9: (this.props.theme.tooltips && this.props.theme.tooltips[8]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[8].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[8]._id
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[8]._id
     },
     comp10: (this.props.theme.tooltips && this.props.theme.tooltips[9]) || {
       competenceId:
         this.props.theme.tooltips && this.props.theme.tooltips[9].competenceId,
-      tooltip: "", _id: this.props.theme.tooltips && this.props.theme.tooltips[9]._id
-    }
+      tooltip: "",
+      _id: this.props.theme.tooltips && this.props.theme.tooltips[9]._id
+	},
+	open: false,
   };
   handleChange1 = (e: any) => {
     this.setState({ comp1: { ...this.state.comp1, tooltip: e.target.value } });
@@ -150,14 +167,32 @@ class TooltipForm extends React.Component<Props, State> {
           this.state.comp10
         ]
       });
-    }
+    } else {
+		this.setState({open: true})
+	}
   };
+  handleClose = () => {
+	this.setState({open: false})
+  }
 
   render() {
     const { competences } = this.props;
     const { classes } = this.props;
     return (
       <div className={classes.container}>
+        <Dialog open={this.state.open} onClose={this.handleClose}>
+		<DialogTitle id="alert-dialog-title">{"Avertissement"}</DialogTitle>
+		<DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            vous devez remplir tous les champs des tooltips
+          </DialogContentText>
+        </DialogContent>
+		<DialogActions>
+          <Button onClick={this.handleClose} color="primary" autoFocus>
+            D'accord
+          </Button>
+        </DialogActions>
+		</Dialog>
         <div className={classes.inputWrapper}>
           <label className={classes.label} htmlFor="">
             {competences[0].title}
